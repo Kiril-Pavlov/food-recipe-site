c34c7d65700fbbf7be37c25e78e4410292c39796
Kiril-Pavlov <Kire_P@yahoo.com>
2022-10-17 18:46:22 +0200

    imported the pages and basic componentsto App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,18 @@
 import './App.css';
+import { Routes, Route } from 'react-router';
+
+//pages
+import Home from "./pages/Home/index"
+import About from "./pages/About/About"
+import Recipes from "./pages/Recipes/Recipes"
+import Favorites from "./pages/Favorites/Favorites"
+import Blog from "./pages/Blog/Blog"
+import Contact from "./pages/Contact/Contact"
+
+//components
+import Header from "./components/Header/Header"
+import Footer from "./components/Footer/Footer"
+import Navbar from "./components/Navbar/Navbar"
 
 function App() {
   return (
