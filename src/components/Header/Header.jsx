1af156855cd6be49dab37a182af92e592c9fdc56
Kiril-Pavlov <Kire_P@yahoo.com>
2022-10-24 00:05:06 +0200

    logic for hamburger button done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import "./Header.css"
 import Navbar from '../Navbar/Navbar'
 
-const Header = ({links}) => {
+import {GiHamburgerMenu} from "react-icons/gi"
+import {AiOutlineClose} from "react-icons/ai"
+
+const Header = ({ links }) => {
+  const [isHamburgerOpened,setIsHamburgerOpened] =useState(false);
+
+  const handleHamburgerOpened = () => {
+    setIsHamburgerOpened(!isHamburgerOpened)
+  }
   return (
     <div className='header-container'>
       <div className='header-logo'>
         LOGO
       </div>
-      <Navbar links={links}/>
+      <div className='navbar-component'>
+        <Navbar links={links}/>
+      </div>
+      <div className='navbar-hamburger-button'>
+        {isHamburgerOpened === true ? (
+          <AiOutlineClose size={25} onClick={handleHamburgerOpened}/>
+        ) : (
+          <GiHamburgerMenu size={25} onClick={handleHamburgerOpened}/>
+        )}
+      </div>
     </div>
   )
 }
